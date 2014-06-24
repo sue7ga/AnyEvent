fb1e4ba268aa8fa742fbdeed03abbbee13438829
sue7ga <sue77ga@gmail.com>
2014-06-24 08:07:20 -0700

    schools

--- shingaku.pl
+++ shingaku.pl
@@ -2,11 +2,12 @@ use strict;
 use warnings;
 use WebService::Simple;
 use XML::Simple;
+use utf8;
 use Carp;
 
 our $apikey = 'ad425e84441a387a';
 our $url = 'http://webservice.recruit.co.jp/shingaku/school/v1/?key=';
-our $keyword = "数学";
+our $keyword = "英語";
 our $pref_cd = 13;
 
 my $lingr = WebService::Simple->new(
@@ -17,8 +18,24 @@ my $lingr = WebService::Simple->new(
 my $response = $lingr->get();
 use Data::Dumper;
 
-print Dumper $response->{_content};
+use XML::Simple;
 
+my $xs = new XML::Simple();
+my $ref = $xs->XMLin($response->{_content});
+
+my @schools = keys $ref->{'school'};
+
+foreach my $school(@schools){
+ print $school,"\n";
+}
+
+my %schools =  map{$ref->{'school'}->{$_}}@schools;
+
+foreach my $key(keys %schools){
+  foreach my $foo(%{$schools{$key}}){
+    #print $schools{$key}{$foo};
+  }
+}
 
 1;
 
